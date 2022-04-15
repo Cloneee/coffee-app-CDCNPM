4bae64e60815e61a7cb977b2069f9865991ec5ee
Clone <40709168+Cloneee@users.noreply.github.com>
2022-04-15 17:03:43 +0700

    Before adding MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#feaa39',
+      light: '#f7b256',
+      dark: '#fb9d1f',
+    },
+    secondary: {
+      main: '#ee571c',
+    },
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
